1cacaef0277f0dc16fce43ee52742a34f934187f
girlshotspretty <luana.lima28@escola.pr.gov.br>
2026-07-02 08:18:32 -0300

    jjkl

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const numerosenha = document.querySelector('.parametro-senha__texto');
 let tamanhosenha = 12;
-numerosenha.textcontent= tamanhosenha;
+numerosenha.textContent= tamanhosenha;
 
 const botoes= document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuitamanho;
-botoes[1].onclik = aumentatamanho;
+botoes[1].onclick = aumentatamanho;
 
 function diminuitamanho(){
     if(tamanhosenha >1 ){
       //  tamanhosenha = tamanhosenha-1;
         tamanhosenha--;
     }
-numerosenha.textcontent= tamanhosenha;
+numerosenha.textContent= tamanhosenha;
 }
 function aumentatamanho(){
     if(tamanhosenha <20 ){
